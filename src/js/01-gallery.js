db353da2016567a0da1caabcdc9d8fcb9fcef1f4
Elena-Holz <97180066+Elena-Holz@users.noreply.github.com>
2022-07-19 20:24:03 +0300

    add19.06

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,34 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from "simplelightbox";
-
+import imgCardTemp from "../templates/gallery1.hbs";
 import "simplelightbox/dist/simple-lightbox.min.css";
+console.log(imgCardTemp(galleryItems[1]));
 
-console.log(galleryItems);
+const divContainer = document.querySelector('.gallery');
+const imgCard = getImgCart(galleryItems);
+divContainer.insertAdjacentHTML('beforeend', imgCard);
+
+function getImgCart(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `
+}).join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "bottom",
+  });
